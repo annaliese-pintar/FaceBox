deb2adc22e5d51d099a66b6d17f71dc7d3eda541
annaliese-pintar <annaliese_pintar@yahoo.com>
2025-03-01 15:19:18 -0800

    Added shut down pi after program runs

--- src/pi_setup.py
+++ src/pi_setup.py
@@ -1,9 +1,13 @@
 import facial_recognition
 import lock_control
 import LED
+import os
 
 if facial_recognition.recognise_face() is True:
     lock_control.lock_function()
     LED.green_LED()
 else:
     LED.red_LED()
+
+# shut down pi
+os.system("sudo shutdown -h now")
